1a21b53eaa8d3af69c865ca18eb0e16f8e1e94d2
MechZombie <dojathan@gmail.com>
2020-03-02 20:57:56 -0300

    Aprendendo a usar SPRINGBOOT

---
+++ src/main/java/com/spring/codeblog/utils/DummyDate.java
@@ -0,0 +1,46 @@
+package com.spring.codeblog.utils;
+
+import com.spring.codeblog.model.Post;
+import com.spring.codeblog.repository.CodeblogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class DummyDate {
+
+    @Autowired
+    CodeblogRepository codeblogRepository;
+
+//    @PostConstruct
+    public void savePosts() {
+
+        List<Post> postList = new ArrayList<>();
+
+        Post post1 = new Post();
+        post1.setAutor("Alberto Paiva");
+        post1.setData(LocalDate.now());
+        post1.setTitulo("Docker");
+        post1.setTexto("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum");
+
+        Post post2 = new Post();
+        post2.setAutor("Alberto Paiva");
+        post2.setData(LocalDate.now());
+        post2.setTitulo("Api REST");
+        post2.setTexto("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum");
+
+        postList.add(post1);
+        postList.add(post2);
+
+        for(Post post: postList){
+            Post postSaved = codeblogRepository.save(post);
+            System.out.println(postSaved.getId());
+        }
+    }
+
+
+}
